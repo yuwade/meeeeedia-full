1dd7aa09483534942c2bfe6f48e9bbff7cacb835
milo <oncwnuGH1xoGMkTdTx3FObsDGYL4@git.weixin.qq.com>
2022-05-07 11:47:03 +0800

    edit

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import naive from 'naive-ui'
 import router from "./router"
-import store from "./store"
+import store from "./stores"
 // 通用字体
 import 'vfonts/Lato.css'
 // 等宽字体
